f09d6430956da7b47ac02f94337626296be64dec
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-10-25 09:42:36 -0300

    Adding boilerplate raylib

--- code/Main.c
+++ code/Main.c
@@ -5,13 +5,56 @@
 #include "cute_tiled.h"
 
 
-int main(int argc, char const *argv[])
-{
-    cute_tiled_map_t* map = cute_tiled_load_map_from_file("../data/export/basic.json", NULL);
+// int main(int argc, char const *argv[])
+// {
+
+
+//     printf("%d\n", layer->data[1119]);
     
+//     return 0;
+// }
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    cute_tiled_map_t* map = cute_tiled_load_map_from_file("../data/export/basic.json", NULL);
     cute_tiled_layer_t* layer = map->layers;
 
-    printf("%d\n", layer->data[1119]);
-    
+    Rectangle srcTilemapRec = { 0, 0, layer->width, layer->height };
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
     return 0;
 }
